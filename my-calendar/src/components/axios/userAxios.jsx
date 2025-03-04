4825e446d1aa5f17ff8e57c415bf64e8882e1e5a
michal bakshi <michalb5980@gmail.com>
2025-03-05 00:17:00 +0200

    change to json  from text  to biuld

--- my-calendar/src/components/axios/userAxios.jsx
+++ my-calendar/src/components/axios/userAxios.jsx
@@ -49,7 +49,9 @@ export const handleLogin = async (e, loginUser) => {
       console.log("Response status:", response.status);
       console.log("Response headers:", response.headers.get("content-type"));
       if (response.status === 200) {
-        return await response.text(); 
+       const text= await response.json(); 
+       console.log(text)
+        return text;
       } else {
         const data = await response.json();
         throw new Error(data.message); 
